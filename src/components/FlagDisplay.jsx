e8331e9d7d112cd28c85ae07b343ccb49686a323
vedkulk <kulkarnived3@gmail.com>
2024-12-20 23:22:52 +0530

    Flag Display

--- src/components/FlagDisplay.jsx
+++ src/components/FlagDisplay.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import flags from '../data/flags.json';
+
+// Example: Accessing the data
 
 const FlagDisplay = () => {
+    console.log(flags[0].name.common);
+    const flagImg = flags[5].flags.svg 
+
   return (
     <div className='mt-10 w-[250px] h-[200px] md:w-[500px] md:h-[330px] border-2 border-main '>
       <img 
-        src='https://flagcdn.com/in.svg' 
+        src={flagImg}
         className="w-full h-full object-cover"
         alt="Indian flag"
       />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-        <div className="w-screen h-[100px] flex flex-col text-center justify-center text-4xl block-inline  border-main bg-stone-700">
+        <div className="w-screen h-[60px] flex flex-col text-left justify-center text-2xl block-inline  border-main bg-stone-700">
             FlagsIO
         </div>
     </>
