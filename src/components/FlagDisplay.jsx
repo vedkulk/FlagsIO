6051c4b375e906649889f04f32c324bc9053f8ec
vedkulk <kulkarnived3@gmail.com>
2024-12-22 23:41:32 +0530

    dimensions

--- src/components/FlagDisplay.jsx
+++ src/components/FlagDisplay.jsx
@@ -3,14 +3,16 @@ import React from 'react';
 const FlagDisplay = ({ currentFlag }) => {
   return (
     <>
-      {currentFlag.svg ? (
-        <img
-          src={currentFlag.svg}
-          className="mt-10 w-[280px] h-[200px] md:w-[500px] md:h-[330px] object-cover rounded-md md:object-cover border-2 border-main"
-        />
-      ) : (
-        <div>Loading...</div> // Show loading if flag is not available
-      )}
+    <div className='mt-10 w-[280px] md:w-[500px] '>
+        {currentFlag.svg ? (
+          <img
+            src={currentFlag.svg}
+            className="w-full h-full object-fit rounded-md md:object-fit border-2 border-main"
+          />
+        ) : (
+          <div>Loading...</div> // Show loading if flag is not available
+        )}
+      </div>
     </>
   );
 };
